e87a4b09c03844fcf7d645102767179569308c31
taitwelve <taynascimento15@gmail.com>
2024-11-08 20:10:10 -0300

    fix: ajustes e finalização do projeto

--- ingresso/js/app.js
+++ ingresso/js/app.js
@@ -2,8 +2,18 @@ function comprar (){
     let tipo = document.getElementById('tipo-ingresso');
     let qtd = parseInt(document.getElementById('qtd').value);
 
+
+    if (isNaN(qtd) || qtd <= 0) {
+        alert('Por favor, insira uma quantidade válida.');
+        return;
+    }
+
     if (tipo.value == 'pista') {
         comprarPista(qtd);
+    } else if (tipo.value == 'superior') {
+        comprarCadeiraSuperior(qtd);
+    } else {
+        comprarCadeiraInferior(qtd);
     }    
 }
 
@@ -14,6 +24,28 @@ function comprarPista(qtd) {
     } else {
         qtdPista = qtdPista - qtd;
         document.getElementById('qtd-pista').textContent = qtdPista;
+        alert('Compra realizada com sucesso!');
+    }
+}
+
+function comprarCadeiraSuperior(qtd) {
+    let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);
+    if (qtd > qtdSuperior) {
+        alert('Quantidade insuficiente de ingressos de Cadeira Superior.');
+    } else {
+        qtdSuperior = qtdSuperior - qtd;
+        document.getElementById('qtd-superior').textContent = qtdSuperior;
+        alert('Compra realizada com sucesso!');
+    }
+}
+
+function comprarCadeiraInferior(qtd) {
+    let qtdInferior = parseInt(document.getElementById('qtd-inferior').textContent);
+    if (qtd > qtdInferior) {
+        alert('Quantidade insuficiente de ingressos de Cadeira Superior.');
+    } else {
+        qtdInferior = qtdInferior - qtd;
+        document.getElementById('qtd-inferior').textContent = qtdInferior;
         alert('Compra realizada com sucesso!');
     }
 }
